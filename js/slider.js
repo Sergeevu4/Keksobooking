6dcdceb6c18385757c4e7c48cdd8f6050c8463b5
Sergeevu4 <sergei.sergeevu4@gmail.com>
2018-12-17 14:17:57 +0300

    Добавил callback в slider + починил ограничение по Y

--- js/slider.js
+++ js/slider.js
@@ -11,13 +11,13 @@
   var MAX_SHIFT_LEFT = 1200;
 
   // Высота острия главного пина
-  var PIN_MAIN_HEIGHT = 19;
+  var PIN_MAIN_HEIGHT_POINTER = 19;
 
   // Главный Pin
   var pinMain = document.querySelector('.map__pin--main');
   pinMain.style.zIndex = '1000';
 
-  // Путь к главному пространству - карта(section) ++
+  /*  // Путь к главному пространству - карта(section) ++
   var map = document.querySelector('.map');
 
   // Контейнер Div Пинов на карте +
@@ -30,10 +30,14 @@
   var formFiltersAdsSelect = document.querySelectorAll('select');
 
   // Все теги fieldset на стайте +
-  var formFieldset = document.querySelectorAll('fieldset');
+  var formFieldset = document.querySelectorAll('fieldset'); */
 
   // Объявления пустого объекта, стартовая точка координат при Drag'n'Drop
   var startCoords = {};
+
+  // Начальное состояние функций: (защита) которые принимают callback - для того, чтобы в случае возникнование ошибки, условие бы не сработало внутри обработчиков
+  var pinMainMouseUpCallback = null;
+  var pinMainMouseMoveCallback = null;
 
   // Обработчик Drag'n'Drop
   pinMain.addEventListener('mousedown', function (evt) {
@@ -48,13 +52,36 @@
     document.addEventListener('mouseup', onPinMainMouseUp);
   });
 
+  // Функция: условие ограничения для главного пина при перемещении (mousemove)
+  function limitPinMainPosition(shift) {
+    if (pinMain.offsetTop + shift.y > MAX_SHIFT_TOP - PIN_MAIN_HEIGHT_POINTER - pinMain.offsetHeight) {
+      pinMain.style.top = (MAX_SHIFT_TOP - PIN_MAIN_HEIGHT_POINTER - pinMain.offsetHeight) + 'px';
+    }
+
+    if (pinMain.offsetTop + shift.y < (MIN_SHIFT_TOP - PIN_MAIN_HEIGHT_POINTER - pinMain.offsetHeight)) {
+      pinMain.style.top = (MIN_SHIFT_TOP - PIN_MAIN_HEIGHT_POINTER - pinMain.offsetHeight) + 'px';
+    }
+
+    if (pinMain.offsetLeft + shift.x < (MIN_SHIFT_LEFT - pinMain.offsetWidth / 2)) {
+      pinMain.style.left = (MIN_SHIFT_LEFT - pinMain.offsetWidth / 2) + 'px';
+    }
+
+    if (pinMain.offsetLeft + shift.x > (MAX_SHIFT_LEFT - pinMain.offsetWidth / 2)) {
+      pinMain.style.left = (MAX_SHIFT_LEFT - pinMain.offsetWidth / 2) + 'px';
+    }
+  }
 
   // (Handler) Функция обработчика события mousemove
   function onPinMainMouseMove(evt) {
     evt.preventDefault();
+    if (pinMainMouseMoveCallback) {
+      pinMainMouseMoveCallback();
+    }
 
-    // импорт c map - window.map
-    window.map.writeАddressFormAds(window.map.getCoordinates());
+    /* // импорт c map - window.map
+        window.map.writeАddressFormAds(window.map.getCoordinates());
+        Убрал так как вставка написанных координат не всегда потребуется в других слайдерах
+    */
 
     var shift = {
       x: evt.clientX - startCoords.x,
@@ -69,30 +96,21 @@
     pinMain.style.top = (pinMain.offsetTop + shift.y) + 'px';
     pinMain.style.left = (pinMain.offsetLeft + shift.x) + 'px';
 
-    if (pinMain.offsetTop + shift.y > MAX_SHIFT_TOP - PIN_MAIN_HEIGHT) {
-      pinMain.style.top = (MAX_SHIFT_TOP - PIN_MAIN_HEIGHT) + 'px';
-    }
-
-    if (pinMain.offsetTop + shift.y < MIN_SHIFT_TOP) {
-      pinMain.style.top = MIN_SHIFT_TOP + 'px';
-    }
-
-    if (pinMain.offsetLeft + shift.x < (MIN_SHIFT_LEFT - pinMain.offsetWidth / 2)) {
-      pinMain.style.left = (MIN_SHIFT_LEFT - pinMain.offsetWidth / 2) + 'px';
-    }
-
-    if (pinMain.offsetLeft + shift.x > (MAX_SHIFT_LEFT - pinMain.offsetWidth / 2)) {
-      pinMain.style.left = (MAX_SHIFT_LEFT - pinMain.offsetWidth / 2) + 'px';
-    }
+    limitPinMainPosition(shift);
   }
 
-  // (Handler) Функция обработчика события mouseup
+  // (Handler) Функция обработчика события mouseup - через callback
   function onPinMainMouseUp(evt) {
     evt.preventDefault();
+    if (pinMainMouseUpCallback) {
+      pinMainMouseUpCallback();
+    }
 
-    if (map.classList.contains('map--faded')) {
+    /* Вся внешняя логика
+
+      if (map.classList.contains('map--faded')) {
       // Импорт с data -  window.data,   window.pin
-      window.pin.addPins(window.data.generatedObjects);
+      window.map.addPins(window.data.generatedObjects);
       map.classList.remove('map--faded');
       formAds.classList.remove('ad-form--disabled');
 
@@ -105,10 +123,25 @@
     }
 
     // импорт c map - window.map
-    pinsContainerMap.addEventListener('click', window.map.onPinClick);
+    pinsContainerMap.addEventListener('click', window.map.onPinClick); */
 
     document.removeEventListener('mousemove', onPinMainMouseMove);
     document.removeEventListener('mouseup', onPinMainMouseUp);
   }
 
+  // Функция callback - рассчета + внесения координат в адрес input
+  function setPinMainMouseMoveCallback(callback) {
+    pinMainMouseMoveCallback = callback;
+  }
+
+  // Функция callback - которая активирует страницу
+  function setPinMainMouseUpCallback(callback) {
+    pinMainMouseUpCallback = callback;
+  }
+
+  // Экспорт функций которые вернут callback из main
+  window.slider = {
+    setPinMainMouseUpCallback: setPinMainMouseUpCallback,
+    setPinMainMouseMoveCallback: setPinMainMouseMoveCallback
+  };
 })();
