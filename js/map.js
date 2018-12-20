159780a6f31bbc8253a1a92aac8dc7a171bbe9fd
Sergeevu4 <sergei.sergeevu4@gmail.com>
2018-12-20 23:19:53 +0300

    Дораб. onPinClick -> pin (map__pin--active) +массив с сервер.

--- js/map.js
+++ js/map.js
@@ -67,38 +67,52 @@
   //   addressFormAds.value = (object.x + ',' + object.y);
   // }
 
+  // Массив с объектами
+  var dataArray = [];
+  // Функция для получения массива с сервера и передача его в onPinClick
+  function setDataArray(array) {
+    dataArray = array;
+    // console.log(dataArray);
+  }
 
   // (Handler) Функция обработчик: по нажатию на пин => отрисовка карточки в HTML
   function onPinClick(evt) {
     var pinsClick = evt.target.closest('.map__pin:not(.map__pin--main)');
+    // pinsClick.classList.add('map__pin--active');
 
     if (pinsClick) {
       // Импорт с data -  window.data
-      var currentInfo = window.data.generatedObjects.filter(function (item) {
+      var currentInfo = dataArray.filter(function (item) {
         return item.id === pinsClick.id;
-      });
+      })[0];
 
-      // window.data.generatedObjects.filter().[i]
-      // Закрытие перед отрисовкой
+      // Удаления перед отрисовкой карточки + удаление класса activePin;
       closeCard();
 
       // Отрисовка
       // Импорт window.card
-      addCard(window.card.create(currentInfo[0]));
+      addCard(window.card.create(currentInfo));
+
+      // Добавление активного класса выбранному пину
+      pinsClick.classList.add('map__pin--active');
 
       // Закрытие карточки по кнопке
       var closeCardButton = document.querySelector('.popup__close');
       closeCardButton.addEventListener('click', closeCard);
     }
+
   }
 
-  // Функция закрытия карточек + Проверка существует ли она вообще, перед закрытием
+  // Удаления активного пина при выборе следующей карточки
   function closeCard() {
     var card = document.querySelector('.map__card');
+    var activePin = document.querySelector('.map__pin--active');
     if (card) {
       map.removeChild(document.querySelector('.map__card'));
+      activePin.classList.remove('map__pin--active');
     }
   }
+
 
   // (Handler) Функция обработчика нажатия по Escape
   function onEscPress(evt) {
