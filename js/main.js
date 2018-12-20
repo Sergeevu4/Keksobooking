cf96fef36f9f76fcc12ebfec5e6ceb53dd5aeb40
Sergeevu4 <sergei.sergeevu4@gmail.com>
2018-12-20 22:46:02 +0300

    Отред. activatePage, Добав deactivatePage + отправ. callback

--- js/main.js
+++ js/main.js
@@ -4,25 +4,26 @@
   // Путь к главному пространству - карта(section) ++
   var map = document.querySelector('.map');
 
-  // Контейнер Div Пинов на карте +
-  var pinsContainerMap = document.querySelector('.map__pins');
-
-  // Форма объявлений +
-  var formAds = document.querySelector('.ad-form');
-
   // Форма фильтрации все теги select (коллекция элементов формы) +
   var formFiltersAdsSelect = document.querySelectorAll('select');
 
   // Все теги fieldset на стайте +
   var formFieldset = document.querySelectorAll('fieldset');
 
+  // var pinsContainerMap = document.querySelector('.map__pins');
 
   // Функция активации страницы в mouseup (callback)
   function activatePage() {
-    // Импорт с data - window.map,  window.data
-    window.map.addPins(window.data.generatedObjects);
-    map.classList.remove('map--faded');
-    formAds.classList.remove('ad-form--disabled');
+
+    // Функция callback получает данные от сервера и отрисовывает их при активации страницы + записывает в них id + вызывает функцию arrayWithId которая отправляет массив в map
+    window.backend.load(function (array) {
+      var arrayWithId = setId(array);
+      window.map.addPins(arrayWithId);
+      // console.log(arrayWithId);
+      window.map.setDataArray(arrayWithId);
+    }, window.message.showMessageError);
+
+    window.map.toggleSiteState();
 
     // импорт c form - window.map (рассчета + внесения координат в адрес input) - при активации страницы, с учетом острия (асинхронно)
     window.form.writeАddressFormAds(window.map.getCoordinates());
@@ -31,9 +32,35 @@
     window.form.toggleForms(formFiltersAdsSelect);
     window.form.toggleForms(formFieldset);
 
-    // импорт c map - window.map
-    pinsContainerMap.addEventListener('click', window.map.onPinClick);
+    // pinsContainerMap.addEventListener('click', window.map.onPinClick);
   }
+
+
+  // Функция дезактивации страницы
+  function deactivatePage() {
+    window.form.onResetFormSite();
+    // window.map.toggleSiteState();
+
+    // // импорт window.form.toggleForms
+    // window.form.toggleForms(formFiltersAdsSelect);
+    // window.form.toggleForms(formFieldset);
+
+    // window.map.removePins();
+    // window.map.closeCard();
+    // window.map.resetMainPinPosition();
+
+    // window.form.writeАddressFormAds(window.map.getCoordinates());
+    // window.form.resetFormAds();
+  }
+
+  // Функция по добавлению id в массив с объектами полученный c сервера
+  function setId(array) {
+    for (var i = 0; i < array.length; i++) {
+      array[i].id = 'map__pin_id' + (i + 1);
+    }
+    return array;
+  }
+
 
   // Функция передачи callback (активации страницы)
   window.slider.setPinMainMouseUpCallback(function () {
@@ -42,9 +69,21 @@
     }
   });
 
+
   // Функция передачи callback (рассчета + внесения координат в адрес input) при каждом движении курсора (асинхронно)
   window.slider.setPinMainMouseMoveCallback(function () {
     window.form.writeАddressFormAds(window.map.getCoordinates());
   });
+
+
+  // Функция передачи callback (дезактивация страницы) при успешной отправки формы
+  window.form.setDeactivatePageCallback(function () {
+    deactivatePage();
+  });
+
+  // Экспорт
+  // window.main = {
+  //   deactivatePage: deactivatePage
+  // };
 
 })();
