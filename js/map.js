3df44a1e2df9e526a8873fc097b9b0a5ce8e8b03
Sergeevu4 <sergei.sergeevu4@gmail.com>
2018-12-27 01:48:05 +0300

    Редакт. название перемен., функций и их месторас. js/map.js

--- js/map.js
+++ js/map.js
@@ -4,9 +4,6 @@
   // Высота острия главного пина +
   var PIN_MAIN_HEIGHT_POINTER = 19;
 
-  // Массив с объектами с сервера
-  var dataArray = [];
-
   // Путь к главному пространству - карта(section) ++
   var map = document.querySelector('.map');
 
@@ -20,7 +17,10 @@
   var pinsContainerMap = document.querySelector('.map__pins');
 
   // Форма объявлений
-  var formAds = document.querySelector('.ad-form');
+  var formAd = document.querySelector('.ad-form');
+
+  // Форма фильтрации объявлений
+  var formFilters = document.querySelector('.map__filters');
 
   // Объект с записанными первоначальными координатами для последующего возврата Главного пина
   var mainPinInitialPosition = {
@@ -28,13 +28,23 @@
     top: pinMain.style.top
   };
 
+  // Массив с объектами с сервера
+  var dataAds = [];
+
+  // Обработчик события клика и последующего закрытия карточки
+  document.addEventListener('keydown', onEscPress);
+
+  // Функция для получения массива с сервера и передача его в onPinClick
+  function setSaveAds(ads) {
+    dataAds = ads;
+  }
 
   // Функция добавления пинов в разметку через фрагмент
-  function addPins(array) {
+  function addPins(ads) {
     var fragmentPin = document.createDocumentFragment();
 
     // Проверка у переданного с сервера массива, есть ли у его объектов ключа .offer
-    var newArray = array.filter(function (elem) {
+    var newArray = ads.filter(function (elem) {
       return elem.offer;
     });
 
@@ -48,10 +58,64 @@
     pinsContainerMap.addEventListener('click', onPinClick);
   }
 
-
   // Функция добавления карточки в разметку
-  function addCard(object) {
-    map.insertBefore(object, mapFilters);
+  function addCard(ad) {
+    map.insertBefore(ad, mapFilters);
+  }
+
+  // Удаления активного пина при выборе следующей карточки
+  function closeCard() {
+    var card = document.querySelector('.map__card');
+    var activePin = document.querySelector('.map__pin--active');
+    if (card) {
+      card.remove();
+      activePin.classList.remove('map__pin--active');
+    }
+  }
+
+  // (Handler) Функция обработчик: по нажатию на пин => отрисовка карточки в HTML
+  function onPinClick(evt) {
+    var pinsClick = evt.target.closest('.map__pin:not(.map__pin--main)');
+
+    if (pinsClick) {
+      var currentAd = dataAds.filter(function (item) {
+        return item.id === pinsClick.id;
+      })[0];
+
+      // Удаления перед отрисовкой карточки + удаление класса activePin;
+      closeCard();
+
+      // Отрисовка
+      addCard(window.card.create(currentAd));
+
+      // Добавление активного класса выбранному пину
+      pinsClick.classList.add('map__pin--active');
+
+      // Закрытие карточки по кнопке
+      var closeCardButton = document.querySelector('.popup__close');
+      closeCardButton.addEventListener('click', closeCard);
+    }
+  }
+
+  // (Handler) Функция обработчика нажатия по Escape
+  function onEscPress(evt) {
+    if (evt.code === 'Escape') {
+      closeCard();
+    }
+  }
+
+  // Функция удаления Пинов при дезактивации страницы и при нажатии на кнопку сбороса
+  function removePins() {
+    var pins = document.querySelectorAll('.map__pin:not(.map__pin--main)');
+    for (var i = 0; i < pins.length; i++) {
+      pins[i].remove();
+    }
+  }
+
+  // Возврат Главного пина в точку его первоначального состояния
+  function resetMainPinPosition() {
+    pinMain.style.left = mainPinInitialPosition.left;
+    pinMain.style.top = mainPinInitialPosition.top;
   }
 
   // Функция подсчета координат для Основного пина
@@ -68,89 +132,19 @@
     return coordinates;
   }
 
-
-  // Функция для получения массива с сервера и передача его в onPinClick
-  function setDataArray(array) {
-    dataArray = array;
-  }
-
-
-  // (Handler) Функция обработчик: по нажатию на пин => отрисовка карточки в HTML
-  function onPinClick(evt) {
-    var pinsClick = evt.target.closest('.map__pin:not(.map__pin--main)');
-    // pinsClick.classList.add('map__pin--active');
-
-    if (pinsClick) {
-      // Импорт с data -  window.data
-      var currentInfo = dataArray.filter(function (item) {
-        return item.id === pinsClick.id;
-      })[0];
-
-      // Удаления перед отрисовкой карточки + удаление класса activePin;
-      closeCard();
-
-      // Отрисовка
-      // Импорт window.card
-      addCard(window.card.create(currentInfo));
-
-      // Добавление активного класса выбранному пину
-      pinsClick.classList.add('map__pin--active');
-
-      // Закрытие карточки по кнопке
-      var closeCardButton = document.querySelector('.popup__close');
-      closeCardButton.addEventListener('click', closeCard);
-    }
-
-  }
-
-  // Удаления активного пина при выборе следующей карточки
-  function closeCard() {
-    var card = document.querySelector('.map__card');
-    var activePin = document.querySelector('.map__pin--active');
-    if (card) {
-      card.remove();
-      activePin.classList.remove('map__pin--active');
-    }
-  }
-
-
-  // (Handler) Функция обработчика нажатия по Escape
-  function onEscPress(evt) {
-    if (evt.code === 'Escape') {
-      closeCard();
-    }
-  }
-
-  // Обработчик события клика и последующего закрытия карточки
-  document.addEventListener('keydown', onEscPress);
-
-  // Функция удаления Пинов при дезактивации страницы и при нажатии на кнопку сбороса
-  function removePins() {
-    var pins = document.querySelectorAll('.map__pin:not(.map__pin--main)');
-
-    for (var i = 0; i < pins.length; i++) {
-      pins[i].remove();
-    }
-  }
-
-  // Функция переключения классов при активации и дезактивации страницы
+  // Функция переключения классов при активации и переключения доступности к формам
   function toggleSiteState() {
     map.classList.toggle('map--faded');
-    formAds.classList.toggle('ad-form--disabled');
-  }
-
-
-  // Возврат Главного пина в точку его первоначального состояния
-  function resetMainPinPosition() {
-    pinMain.style.left = mainPinInitialPosition.left;
-    pinMain.style.top = mainPinInitialPosition.top;
+    formAd.classList.toggle('ad-form--disabled');
+    window.formAd.toggle(formAd);
+    window.formAd.toggle(formFilters);
   }
 
   // Экспорт
   window.map = {
     addPins: addPins,
     getCoordinates: getCoordinates,
-    setDataArray: setDataArray,
+    setSaveAds: setSaveAds,
     closeCard: closeCard,
     removePins: removePins,
     toggleSiteState: toggleSiteState,
